e5e1179c446df4c4a3d0611d70f74b35cae04933
Marek Tomas <sogysvk@gmail.com>
2020-09-18 12:27:47 +0200

    stream API testing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import entity.Person;
+
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -40,6 +42,48 @@ public class Main {
         System.out.println("\nshow list parallel stream");
         listOf
                 .parallelStream()
+                .sorted()
                 .forEach(System.out::println);
+
+
+        List<Person> persons =
+                Arrays.asList(
+                        new Person("Max", 18),
+                        new Person("Peter", 23),
+                        new Person("Pamela", 23),
+                        new Person("David", 12));
+
+        List<Person> filtered =
+                persons
+                        .stream()
+                        .filter(p -> p.getName().startsWith("P"))
+                        .collect(Collectors.toList());
+
+        System.out.println(filtered);
+        System.out.println();
+
+        Person result =
+                persons
+                        .stream()
+                        .reduce(new Person("", 0), (p1, p2) -> {
+                            p1.setAge(p1.getAge() + p2.getAge());
+                            p1.setName(p1.getName() + p2.getName());
+                            return p1;
+                        });
+
+        System.out.format("name=%s; age=%s", result.getName(), result.getAge());
+        Integer ageSum = persons
+                .stream()
+                .reduce(0,
+                        (sum, p) -> {
+                            System.out.format("accumulator: sum=%s; person=%s\n", sum, p);
+                            return sum += p.getAge();
+                        },
+                        (sum1, sum2) -> {
+                            System.out.format("combiner: sum1=%s; sum2=%s\n", sum1, sum2);
+                            return sum1 + sum2;
+                        });
+
+
     }
 }
